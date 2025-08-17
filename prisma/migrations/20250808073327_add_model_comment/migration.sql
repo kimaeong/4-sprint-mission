d180bb21c8035a0a8fb63921f8340d885cf0fd76
kimae-ong <hangayun1027@naver.com>
2025-08-17 17:22:25 +0900

    Remove all files from main branch

--- prisma/migrations/20250808073327_add_model_comment/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."comment" (
-    "id" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "comment_pkey" PRIMARY KEY ("id")
-);
--- prisma/mock.js
+++
@@ -1,9 +0,0 @@
-export const PRODUCT = [
-   {
-    name: '츄르 셋트',
-    description: '닭가슴살 맛',
-    price: 12000,
-    tags: ['고양이', '간식']
-   },
-]
-
